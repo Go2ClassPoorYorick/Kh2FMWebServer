aefae35a72bc4f83a983ea25863a8a816c6a898e
= <=>
2022-03-21 12:33:58 -0400

    Added an ability clear function and positionally aware ability  writing functionality

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ from flask import Flask
 
 # Reads ability memory and attempts to map abilities in memory to known database generated above
 def getAbilities():
-    abilities_bytes = pm.read_bytes(abilities_start,abilities_length)
-
+    abilities_bytes = pm.read_bytes(abilities_start,abilities_length+2)
+    
   
     equipped_abilities = [] 
     # Equipped abilities will store the list of abilities we've found in memory
@@ -68,12 +68,15 @@ def getAbilities():
     while i < len(abilities_bytes):
         ability = abilities_bytes[i:i+2][::-1]
         ability_str = hex(int.from_bytes(ability,"big") & int.from_bytes(b'\x7F\xFF',"big"))[2:].zfill(4) # Remove equipped bit flag
-
+        memLocation = hex(abilities_start+i)
         i=i+2
         try:
-            equipped_abilities.append(shittyWayToMapAbilities[ability_str.upper()])
+            if int.from_bytes(ability,"big") != 0:
+                equipped_abilities.append([memLocation, shittyWayToMapAbilities[ability_str.upper()]])
+            else:
+                equipped_abilities.append([memLocation, {}])
         except(KeyError):
-            continue
+            equipped_abilities.append([memLocation, {"Id":hex(int.from_bytes(ability,"big"))}])
     return equipped_abilities
 
 # People like to assume that a server has to have some webpage- we provide one, and an elementary interface for manually interacting with the api.
@@ -93,29 +96,41 @@ def generate():
 # A wrapper to run getAbilities and return it formatted
 @app.route('/readAbilities')
 def read():
-    return str(getAbilities()).replace('}, {','<br>')
+    return str(getAbilities()).replace('], [','],<br>[')
 
 # This cluster allows us to write abilities... see readme
 @app.route('/writeAbility', methods = ['POST','GET'])
 def writeAbilities():
     oldAbilityList = getAbilities()
     abilities_to_write = request.args.getlist('ability')
-    byteChart = []
-    print(len(oldAbilityList))
-    offset = 0x2032E074 + len(oldAbilityList)*2
-    print(offset)
+    emptyMemItr = (location for location in oldAbilityList if location[1] == {}) #Allows us to use the next(emptyMemItr) function to access memory locations for writing
+
     for ability in abilities_to_write:
-        if tiered_abilities_chart.get(str(ability)) == None:
-            # TODO: Workout avoiding rando-protected memory ranges
-            print(ability)
-            pm.write_bytes(offset,bytes.fromhex(ability.zfill(4))[::-1],2) #[::-1] reverses byte order, allowing me to use traditionally written item ids to write to memory
-            offset += 2 
-        else:
-            print(f'ability:{ability} skipped due to lack of "scaled ability" logic')
-            # TODO: count existing instances of each type of tiered ability, and sum them.   
-            #character_count_address = pymem.pattern.scan_pattern_page(pm.process_handle, address_reference, bytes_pattern)
+        try:
+            if tiered_abilities_chart.get(str(ability)) == None:
+                nextEmptySlot = next(emptyMemItr)[0]
+                print (nextEmptySlot, ability.zfill(4))
+                pm.write_bytes(
+                    int(nextEmptySlot,16), # Reads memory location from string
+                    bytes.fromhex(ability.zfill(4))[::-1], # [::-1] reverses byte order, allowing me to use traditionally written item ids to write to memory
+                    2 # Bytes to write (this is silly, why wouldn't pymem just calculate that from above???)
+                    ) 
+            else:
+                print(f'ability: {ability} skipped due to lack of "scaled ability" logic')
+                # TODO: count existing instances of each type of tiered ability, and sum them.   
+                #character_count_address = pymem.pattern.scan_pattern_page(pm.process_handle, address_reference, bytes_pattern)
+        except(StopIteration):
+            print("Couldn't write any new abilities, ability list full")
     return str(getAbilities()).replace('}, {','<br>')
-    
+
+@app.route('/clearAbility', methods = ['POST','GET'])
+def clearAbilities():
+    pm.write_bytes(
+    abilities_start, # Clear from the start of the memory range
+    bytes.fromhex(''.zfill((abilities_length+1)*2)), # Lotsa 0s
+    abilities_length+2 # Clear ALL
+    )
+    return str(getAbilities()).replace('], [','],<br>[')
 
 if __name__ == '__main__':
     app.run(...)
